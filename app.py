1c6c287306003f45363efd3ab86cd9f5baae5496
sai8151 <saikiranreddy8151@gmail.com>
2023-10-18 23:24:07 +0530

    initial commits

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from flask import Flask, request, render_template
+from gensim.summarization import summarize
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/summarize', methods=['POST'])
+def summarize_text():
+    text = request.form['text']
+    summary = summarize(text)
+    return render_template('summary.html', summary=summary, original_text=text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
