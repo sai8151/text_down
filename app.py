f510ed358a8bedf539f035e99631908277eb9193
sai8151 <saikiranreddy8151@gmail.com>
2023-11-20 10:04:20 +0530

    #Nov_20

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import random
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,19 +19,18 @@ def index():
 #     summary = summarize(text)
 #     return render_template('summary.html', summary=summary, original_text=text)
 
+
 @app.route('/upload', methods=['POST'])
 def success():
     if request.method == 'POST':
         f = request.files['file']
         f.save(f.filename)
-
         # Set the path to your PDF file and the output PPTX file in a temporary directory
         pdf_path = f.filename
         output_pptx_path = tempfile.mktemp(suffix=".pptx")
-
         create_ppt_from_summarized_content(pdf_path, output_pptx_path)
-
         return send_file(output_pptx_path, as_attachment=True, download_name="output.pptx")
+
 
 def extract_headings_and_content(pdf_path):
     headings_and_content = []
@@ -51,32 +51,39 @@ def extract_headings_and_content(pdf_path):
                     headings_and_content.append((headings, content))
     return headings_and_content
 
+
 def create_ppt_from_summarized_content(pdf_path, output_pptx_path):
     headings_and_content = extract_headings_and_content(pdf_path)
     presentation = Presentation()
-    
+
     # Define the maximum number of slides to generate
-    max_slides = 5  # You can adjust this as needed
-    
+    max_slides = 20  # You can adjust this as needed
+
     # Shuffle the order of content for randomness
     random.shuffle(headings_and_content)
-    
+
     for i, (headings, content) in enumerate(headings_and_content):
         if i >= max_slides:
             break  # Stop after generating the maximum number of slides
-        
-        slide = presentation.slides.add_slide(presentation.slide_layouts[5])  # Blank slide layout
+
+        slide = presentation.slides.add_slide(
+            presentation.slide_layouts[5])  # Blank slide layout
         if headings:
             title = slide.shapes.title
-            title.text = headings[0]  # Use the first heading as the slide title
+            # Use the first heading as the slide title
+            title.text = headings[0]
         if content:
             word_count = random.randint(100, 150)
-            summarized_content = summarize("\n".join(content), word_count=word_count)            
-            text_box = slide.shapes.add_textbox(Inches(1), Inches(1), Inches(8), Inches(5))
+            summarized_content = summarize(
+                "\n".join(content), word_count=word_count)
+            text_box = slide.shapes.add_textbox(
+                Inches(1), Inches(1), Inches(8), Inches(5))
             text_frame = text_box.text_frame
             p = text_frame.add_paragraph()
             p.text = summarized_content
     presentation.save(output_pptx_path)
 
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
+    app.run()
